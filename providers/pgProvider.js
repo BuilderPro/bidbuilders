a05f4b4c91f2dcee2bb312f923485e93681d3e42
Jordan Wild <jwild@simplifile.com>
2015-09-24 23:21:39 -0600

    adding knex

--- providers/pgProvider.js
+++ providers/pgProvider.js
@@ -1,24 +1,9 @@
-var pg = require('pg');
-
 var conString = "postgres://postgres:"+process.env.PSQL_PASS+"@localhost/bbdb";
 
-var connect = function(response, sql, params, callback) {
-	pg.connect(conString, function(err, client, done) {
-		client.query(sql, params, function(err, result) {
-			if(err) {
-				console.log(err)
-				response.writeHead(500, {'content-type': 'text/plain'});
-				response.end('A databse error occurred');
-			} else if(callback)
-				callback(response, result);
-			else if(params && result.rows.length == 1)
-				response.json(result.rows[0]);
-			else
-				response.json(result.rows);
-			done();
-		});
-	});	
-}
+var db = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING
+});
 
 // var getTeam = function(response, person_id) {
 // 	var column = isNaN(person_id) ? 'name' : 'person_id';
@@ -34,20 +19,11 @@ var connect = function(response, sql, params, callback) {
 // }
 
 module.exports = {
-	getUser: function(response, email) {
-		var sql = 'SELECT * FROM users WHERE email = $1';
-		connect(response, sql, [email]) 
+	getUser: function(email) {
+		return db('users').where('email', 1)
 	},
 	createUser: function(response, user) {
-		var params = [
-			user.email,
-			user.name,
-			user.stuff
-		];
-		var sql = 'INSERT INTO users (email,name,stuff) ' + 
-			'VALUE ($1,$2,$3) RETURNING *';
-
-		connect(response, sql, params);
+		db('users').insert(user)
 	}
 }
 
