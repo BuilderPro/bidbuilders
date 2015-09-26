3fc356b3edb7e3fc05c66827cc344240770e4365
Dylan Lott <lott.dylan@gmail.com>
2015-09-26 14:35:45 -0600

    added user state to the navbar

--- public/app/dashboard/dashboardCtrl.js
+++ public/app/dashboard/dashboardCtrl.js
@@ -7,12 +7,14 @@ app
   .controller('DashboardCtrl', ['$scope', '$http', '$state', function($scope, $http, $state, ProjectService) {
 
   	$http.get('/user').then(function(user){
-  		$scope.user = user; 
+  		$scope.user = user.data; 
 	}, function(err){
 		if(err) console.log("get User err: ", err); 
 	});
-	
-    console.log("DashboardCtrl");
+
+    $http.get('/projects').then(function(projects){
+    	$scope.projects = projects.data; 
+    }); 
 
 
 }]);
